8b1c4185456a93f424b0ae1c16c1a39b2d627130
Shaun Francis <Shaun.Francis@hmcts.net>
2017-03-13 16:04:53 +0000

    Move exception into api and remove unused dependencies from poms

--- framework-api/framework-api-common/src/main/java/uk/gov/justice/services/common/configuration/CommonValueAnnotations.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.services.common.configuration;
-
-public final class CommonValueAnnotations {
-
-    static final String NULL_DEFAULT = "_null_default";
-
-    private CommonValueAnnotations() {
-    }
-}
